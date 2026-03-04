ce01746c065db0e42519ba87254457e2b4cf8bc4
Lemmy <studio@quadbyte.net>
2026-03-04 16:03:34 -0500

    bin/dump: improved script to support optional ID

--- bin/dump.sh
+++ bin/dump.sh
@@ -1,1 +1,3 @@
-{ coredumpctl info qs; coredumpctl debug qs -A "-batch -ex 'thread apply all bt full' -ex q" 2>&1; } | tee ~/qs-crash-$(date +%s).txt
+#!/bin/bash
+ID="${1:-qs}"
+{ coredumpctl info "$ID"; coredumpctl debug "$ID" -A "-batch -ex 'thread apply all bt full' -ex q" 2>&1; } | tee ~/qs-crash-$(date +%s).txt
